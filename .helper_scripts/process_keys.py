8d6d96f3a43b327225d8ba3249d7670c4b415f57
bohnerjosh <Squash0002@gmail.com>
2022-07-14 17:28:42 -0500

    added git repo auth code

--- .helper_scripts/process_keys.py
+++ .helper_scripts/process_keys.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from os import system
 
 GITHUB_SSH_PATH = Path.home() / ".ssh/id_ed25519.pub"
 OPENSSH_KEY_PATH = Path.home() / ".ssh/id_ed25519"
@@ -8,9 +9,11 @@ KEY_BEGIN = "-----BEGIN OPENSSH PRIVATE KEY-----"
 KEY_END = "-----END OPENSSH PRIVATE KEY-----"
 data = ""
 
+GIT_USERNAME = input("What is your Github Username? ")
+GIT_EMAIL = input("What is your Github email? ")
 SYM_KEY = input("Paste your SSH Private key for github here: ")
 PRIVATE_KEY = input("Paste your OpenSSH Private key here: ")
-PASSPHRASE = input("Paste yoru Private key's passphrase here: ")
+PASSPHRASE = input("Paste your Private key's passphrase here: ")
 
 lines = PRIVATE_KEY.split()
 
@@ -25,3 +28,6 @@ with open(str(GITHUB_SSH_PATH), "w+") as f:
 
 with open(str(PASSPHRASE_PATH), "w+") as f:
     f.write(PASSPHRASE)
+
+system(f"git config --global user.name {GIT_USERNAME}")
+system(f"git config --global user.email {GIT_EMAIL}")
