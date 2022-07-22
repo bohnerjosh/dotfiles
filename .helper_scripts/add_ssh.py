1bc380a9a105790d3b06af61a01ddd52924bebda
bohnerjosh <Squash0002@gmail.com>
2022-07-22 12:50:17 -0500

    moved files to parent dir

--- .helper_scripts/add_ssh.py
+++
@@ -1,7 +0,0 @@
-from os import system
-from pathlib import Path
-
-KEYFILE_PATH = str(Path.home() / ".ssh/id_ed25519")
-
-command = f"cd ~/.dotfiles/.helper_scripts && DISPLAY=1 SSH_ASKPASS='./process_secret.sh' ssh-add {KEYFILE_PATH} < /dev/null 2>/dev/null"
-system(command)
